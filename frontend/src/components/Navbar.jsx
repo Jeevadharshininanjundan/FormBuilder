e16951edac56ae12a194a0a39303bb280f2fb842
Jeevadharshininanjundan <jdharshininanjundan@gmail.com>
2025-08-11 13:22:35 +0530

    comments

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useUser } from '../context/AuthContext.jsx';
+
 
 const Navbar = () => {
   const { user } = useUser();
