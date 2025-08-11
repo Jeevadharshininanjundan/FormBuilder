6f9426deab0d33a174f44f42cf98e2bfe2924178
Jeevadharshininanjundan <jdharshininanjundan@gmail.com>
2025-08-11 13:18:42 +0530

    register remove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Builder from './pages/Builder';
 import AppLayout from './AppLayout';
 import PreviewFill from './pages/PreviewFill';
-import Register from './pages/Register';
-import Login from './pages/Login';
+
 import Home from './pages/Home';
 import './App.css'
 
