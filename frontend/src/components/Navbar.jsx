489be331aba764dedeaba5b5ddfa3525592de0f5
Jeevadharshininanjundan <jdharshininanjundan@gmail.com>
2025-08-11 13:42:04 +0530

    user

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
-  const { user } = useUser();
+
 
   return (
     <nav className="bg-[#116466] text-white px-6 py-4 flex justify-between items-center fixed top-0 left-0 right-0 z-50 shadow-md border-b border-[#FF6F91]">
